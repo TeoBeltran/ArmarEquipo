f7cea83d9c2ea7e57ba5580f6ba641c6f39c92e7
Teo Beltran <teo.belnan@gmail.com>
2024-03-26 22:35:15 -0300

    Funcion borrar

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
         return array;
     }
 
-    // Función para asignar nombres aleatorios a los equipos
-    function asignarNombresAleatorios() {
+    // Función para asignar a los jugadores
+    function asignarJugadores() {
         const nombresJugadores = Array.from(document.querySelectorAll(".left input"))
                                     .map(input => input.value.trim())
                                     .filter(nombre => nombre !== "");
@@ -27,7 +27,24 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    // Función para borrar los jugadores
+    function borrarJugadores() {
+        const inputsJugadores = document.querySelectorAll(".left input");
+        inputsJugadores.forEach(input => {
+            input.value = ""; // Borrar el valor del input
+        });
+
+        const jugadoresEquipo = document.querySelectorAll(".right .team a");
+        jugadoresEquipo.forEach(jugador => {
+            jugador.textContent = ""; // Borrar el contenido del elemento a
+        });
+    }
+
     // Agregar un listener al botón "Confirmar"
     const botonConfirmar = document.querySelector(".btnMezclar");
-    botonConfirmar.addEventListener("click", asignarNombresAleatorios);
+    botonConfirmar.addEventListener("click", asignarJugadores);
+
+    // Agregar un listener al botón "Borrar"
+    const botonBorrar = document.querySelector(".btnBorrar");
+    botonBorrar.addEventListener("click", borrarJugadores);
 });
